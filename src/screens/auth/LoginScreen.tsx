6800313a64672931730ffaebe08a6717d465a6bf
User-P <pether.1994@gmail.com>
2022-01-27 15:02:11 -0600

    feat:Login without styles

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,6 +1,66 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useForm} from '../../hooks/useForm';
 
-export const LoginScreen = () => {
-  return <Text>Login</Text>;
+interface Props extends NativeStackScreenProps<any, any> {}
+
+export const LoginScreen = ({navigation}: Props) => {
+  const {email, password, onChange} = useForm({
+    email: '',
+    password: '',
+  });
+
+  const onLogin = () => {
+    console.log('Login');
+    Keyboard.dismiss();
+  };
+
+  return (
+    <>
+      <KeyboardAvoidingView style={{flex: 1}} behavior="height">
+        <View>
+          <Text>Login</Text>
+          <Text>Email:</Text>
+          <TextInput
+            placeholder="Ingrese email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={value => onChange(value, 'email')}
+            value={email}
+            onSubmitEditing={onLogin}
+          />
+
+          <Text>Contraseña:</Text>
+          <TextInput
+            placeholder="********"
+            secureTextEntry
+            onChangeText={value => onChange(value, 'password')}
+            onSubmitEditing={onLogin}
+            value={password}
+          />
+
+          <View>
+            <TouchableOpacity onPress={onLogin}>
+              <Text>Login</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <TouchableOpacity onPress={() => navigation.replace('Register')}>
+              <Text>Crear cuenta</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
